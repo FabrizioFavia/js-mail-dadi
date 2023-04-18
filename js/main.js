47f9635681def8466f3a0145a8229297d197f496
FabrizioFavia <personal.favia@gmail.com>
2023-04-18 16:19:54 +0200

    fix(js): fixed for loop

--- js/main.js
+++ js/main.js
@@ -1,18 +1,16 @@
 function verifyEmail() {
 
-    const signedMail = ["personal.favia@gmail.com", "mario.rossi@gmail.com", "rosa.verdi@gmail.com"];
+    const signedMail = ["personal.favia@gmail.com","mario.rossi@gmail.com","rosa.verdi@gmail.com"];
     let userMail = document.getElementById("mailInput").value;
-    let insertedMail = [userMail]
 
-    for (let i = 0; i < insertedMail.length; i++) {
-        let possibleEmail = insertedMail[i];
-        let verifiedEmail = signedMail[i]
+    for (let i = 0; i < signedMail.length; i++) {
+        let verifiedEmail = signedMail[i];
 
-        if (possibleEmail == verifiedEmail) {
-            alert("Email esistente")
-
-        } else {
-            alert("Email non registrata")
+        if (userMail == verifiedEmail) {
+            alert("ciao")
+            
+        } else{
+            alert("email non registrata")
         }
-    };
+};
 };
